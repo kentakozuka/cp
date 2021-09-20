0f26e838b4c36e27a9bd9cd9d7aeed4db617b576
Kenta Kozuka <kentakozuka@users.noreply.github.com>
2021-09-20 21:11:07 +0900

    add permutation

---
+++ lib/permutation.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+typedef long long ll;
+
+// 順列（文字列）
+// O(n!)
+void print_string_permutations(string str) {
+  sort(str.begin(), str.end());
+  do {
+    cout << str << endl;
+  } while (next_permutation(str.begin(), str.end()));
+}
+
+// 順列（数値）
+// O(n!)
+void print_int_permutations(vector<int> nums) {
+  sort(nums.begin(), nums.end());
+  do {
+    for (auto num : nums) {
+      cout << num << " ";
+    }
+    cout << "\n";
+    // 順列の最後になるまで one_case を並び替えながらループ
+  } while (next_permutation(nums.begin(), nums.end()));
+}
+
+int main() {
+  print_string_permutations("str");
+  print_int_permutations({1, 2, 3});
+  return 0;
+}
