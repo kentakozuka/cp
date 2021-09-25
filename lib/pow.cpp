604286ac1eae95082443eff3431fbfdd5cf574f5
Kenta Kozuka <kentakozuka@users.noreply.github.com>
2021-09-25 15:29:42 +0900

    refactor pow

--- lib/pow.cpp
+++ lib/pow.cpp
@@ -1,17 +1,27 @@
+#include <iostream>
+
 using namespace std;
 
 using ll = long long;
 
 // 繰り返し二乗法
-// ビルトインpowは浮動小数点で実装されているため、値が大きいと誤差が出る。
+// 時間計算量: O(logN)
+// 空間計算量: O(1)
+// 指数を2の累乗の積に分解し、計算を効率化する
+// ビルトインpowは浮動小数点で実装されているため、誤差が出る。
+// 実際は実装はnを2進数でみて、下からi桁目が1ならx^(2^i)をかける
 ll POW(ll a, ll n) {
-  ll ans = 1;
+  ll ret = 1;
   while (n > 0) {
-    if (n & 1) {
-      ans = ans * a;
-    }
-    a = a * a;
-    n /= 2;
+    // n の最下位bitが 1 ならば x^(2^i) をかける
+    if (n & 1) ret *= a;
+    a *= a;
+    // n を1bit 左にずらす
+    n >>= 1;
   }
-  return ans;
+  return ret;
+}
+
+int main() {
+  cout << POW(3, 21) << endl;
 }
