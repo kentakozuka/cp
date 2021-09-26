7a166a7889fe16bc16fc27b355c85cb6e4402a19
Kenta Kozuka <kentakozuka@users.noreply.github.com>
2021-09-26 09:11:52 +0900

    refactor

--- test/pow_test.cpp
+++ test/pow_test.cpp
@@ -2,7 +2,6 @@
 #include <gtest/gtest.h>
 
 TEST(pow, BasicAssertions) {
-  // POW
   EXPECT_EQ(POW(2, 10), 1024);
   EXPECT_EQ(POW(5, 8), 390625);
 }
--- test/prime_number_test.cpp
+++ test/prime_number_test.cpp
@@ -1,8 +1,7 @@
 #include "lib/prime_number.hpp"
 #include <gtest/gtest.h>
 
-TEST(prime_number, BasicAssertions) {
-  // is_prime
+TEST(is_prime, BasicAssertions) {
   EXPECT_EQ(is_prime(2), true);
   EXPECT_EQ(is_prime(3), true);
   EXPECT_EQ(is_prime(4), false);
